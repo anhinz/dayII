9036d63ddc62284c5145a3279c829df8fde61205
anhinz <anjahinz@ymail.com>
2022-03-09 19:42:11 +0100

    added file Main.java, that scanns for name, surname and age. + a "hello"

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoje imię ");
+        String name = scanner.nextLine();
+        System.out.println("Jak masz na nazwisko?");
+        String lastname = scanner.nextLine();
+        System.out.println("Podaj swój wiek ");
+        String age = scanner.nextLine();
+        System.out.println("Cześć " + name + " " + lastname);}
 }
